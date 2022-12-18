248ab397d35f58074679881aca71b55703b98bb2
Maxim_shmidt <history1266@mail.ru>
2022-12-18 11:47:21 +0700

    Задача 41

---
+++ task41/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    count++;   
+}
+Console.WriteLine($"[{string.Join(",", array)}]");
+Console.WriteLine(count);
