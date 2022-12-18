7a44d95889aef3220316c2dae48c756fbd3cd836
Maxim_shmidt <history1266@mail.ru>
2022-12-18 17:57:12 +0700

    задача 43

---
+++ task43/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1; y = k2 * x + b2;
+Console.WriteLine($"({string.Join(";", y, x)})");
